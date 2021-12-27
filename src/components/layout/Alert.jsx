e93146f483784dbd2d94ce3d8d54e272f78dbe3e
Guus Frenken <g.frenken@qteco.nl>
2021-12-27 11:19:45 +0100

    Refactor GithubContext

--- src/components/layout/Alert.jsx
+++ src/components/layout/Alert.jsx
@@ -6,7 +6,7 @@ function Alert() {
 
   return (
     alert !== null && (
-      <p className='flex items-start mb-4 space-x-2'>
+      <div className='flex items-start mb-4 space-x-2'>
         {alert.type === 'error' && (
           <svg className='w-6 h-6 flex-none mt-0.5' fill='none' viewBox='0 0 24 24'>
             <circle cx='12' cy='12' r='12' fill='#FECDD3'></circle>
@@ -16,7 +16,7 @@ function Alert() {
         <p className='flex-1 text-base front-semibold leading-7 text-white'>
           <strong>{alert.msg}</strong>
         </p>
-      </p>
+      </div>
     )
   );
 }
--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,31 +1,33 @@
 import {useState, useContext} from 'react';
 import GithubContext from '../../context/github/GithubContext';
+import {searchUsers} from '../../context/github/GithubActions';
 import AlertContext from '../../context/alert/AlertContext';
 
 function UserSearch() {
   const [text, setText] = useState('');
 
-  const {users, searchUsers, clearUsers} = useContext(GithubContext);
+  const {users, dispatch} = useContext(GithubContext);
   const {setAlert} = useContext(AlertContext);
 
   const handleChange = (e) => {
     setText(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (text === '') {
-      //alert('Please enter something');
       setAlert('Please enter something', 'error');
     } else {
-      searchUsers(text);
+      const users = await searchUsers(text);
+      dispatch({type: 'SET_LOADING'});
+      dispatch({type: 'GET_USERS', payload: users});
       setText('');
     }
   };
 
   const handleClearUsers = () => {
-    clearUsers();
+    dispatch({type: 'CLEAR_USERS'});
   };
 
   return (
