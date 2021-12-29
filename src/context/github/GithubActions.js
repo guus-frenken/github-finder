fb4ca339094eeeac9653a986b102f5e43f0ffd1b
Guus Frenken <g.frenken@outlook.com>
2021-12-29 14:34:17 +0100

    Fix baseURL

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -4,9 +4,8 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const github = axios.create({
-  headers: {
-    Authorization: `token ${GITHUB_TOKEN}`,
-  },
+  baseURL: GITHUB_URL,
+  headers: {Authorization: `token ${GITHUB_TOKEN}`},
 });
 
 // Search users
@@ -15,15 +14,15 @@ export const searchUsers = async (text) => {
     q: text,
   });
 
-  const response = await github.get(`${GITHUB_URL}/search/users?${params}`);
+  const response = await github.get(`/search/users?${params}`);
   return response.data.items;
 };
 
 // Get user and repos
 export const getUserAndRepos = async (login) => {
   const [user, repos] = await Promise.all([
-    github.get(`${GITHUB_URL}/users/${login}`),
-    github.get(`${GITHUB_URL}/users/${login}/repos`),
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`)
   ]);
 
   return {user: user.data, repos: repos.data};
